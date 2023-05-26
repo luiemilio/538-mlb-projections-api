4d073ae43cdd936b8a10689f071b2061060e13ad
Luis Espinola <lespinola@gmail.com>
2023-05-26 16:26:13 -0400

    refactor

--- src/utils.ts
+++ src/utils.ts
@@ -9,7 +9,7 @@ const resetDate = (date: Date): Date => {
     return date;
 };
 
-export const getCurrentDate = (): Date => {
+const getCurrentDate = (): Date => {
     const currentDate = new Date();
     return resetDate(currentDate);
 };
@@ -18,16 +18,16 @@ export const normalize538DateString = (dateStr: string): string => {
     return dateStr.replace(/-/g, '/');
 };
 
-export const stringToDate = (dateStr: string): Date => {
+const stringToDate = (dateStr: string): Date => {
     const date = new Date(dateStr);
     return resetDate(date);
 };
 
-export const dateToString = (date: Date): string => {
+const dateToString = (date: Date): string => {
     return [date.getFullYear(), padTo2Digits(date.getMonth() + 1), padTo2Digits(date.getDate())].join('-');
 };
 
-export const date538StringToDate = (dateStr: string): Date => {
+const date538StringToDate = (dateStr: string): Date => {
     const normalizedString = normalize538DateString(dateStr);
     const date = stringToDate(normalizedString);
     return resetDate(date);
@@ -36,11 +36,6 @@ export const date538StringToDate = (dateStr: string): Date => {
 export const getCurrentDateString = (): string => {
     const currentDate = getCurrentDate();
     return dateToString(currentDate);
-};
-
-export const gameIsToday = (gameData: GameDataInfo<Sport>): boolean => {
-    const currentDate = dateToString(getCurrentDate());
-    return normalize538DateString(gameData.date) === currentDate;
 };
 
 export const sortDates = (a: string, b: string): number => {
